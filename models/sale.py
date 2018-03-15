73723292f0224b4c53d9b4210dd46a9c266880c5
Carlos Fonseca <cfonseca@odoogap.com>
2018-03-15 17:29:09 +0000

    [FIX] singleton

--- models/sale.py
+++ models/sale.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
-
 from odoo import api, fields, models
 
+
 class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
+    _inherit = 'sale.order.line'
 
     @api.multi
     def _action_launch_procurement_rule(self):
-        if self.env['ir.config_parameter'].sudo().get_param('procurement_paid.check_sale', default=False):
+        for rec in self:
+            if self.env['ir.config_parameter'].sudo().get_param('procurement_paid.check_sale', default=False):
+                orders = list(set(x.order_id for x in rec))
+                for order in orders:
+                    invoices = order.invoice_ids
 
-            orders = list(set(x.order_id for x in self))
-            for order in orders:
-                invoices = order.invoice_ids
-
-                if (invoices and all([x.residual <= 0 and x.state != 'draft' for x in invoices])) or self.env.context.get('btn_procur') is True:
-                    return super(SaleOrderLine, self)._action_launch_procurement_rule()
-        else:
-            return super(SaleOrderLine, self)._action_launch_procurement_rule()
+                    if (invoices and all(
+                            [x.residual <= 0 and x.state != 'draft' for x in invoices])) or self.env.context.get(
+                        'btn_procur') is True:
+                        return super(SaleOrderLine, self)._action_launch_procurement_rule()
+            else:
+                return super(SaleOrderLine, self)._action_launch_procurement_rule()
 
 
 class account_payment(models.Model):
@@ -38,12 +40,15 @@ class account_payment(models.Model):
 
 
 class SaleOrder(models.Model):
-    _inherit = "sale.order"
+    _inherit = 'sale.order'
 
-    btn_procur = fields.Boolean(compute="compute_module_active")
+    btn_procur = fields.Boolean(compute='compute_module_active')
 
+    @api.multi
     def compute_module_active(self):
-        self.btn_procur = self.env['ir.config_parameter'].sudo().get_param('procurement_paid.check_sale', default=False)
+        IrConfigParameterSudo = self.env['ir.config_parameter'].sudo()
+        for rec in self:
+            rec.btn_procur = IrConfigParameterSudo.get_param('procurement_paid.check_sale', default=False)
 
     def action_ignore_payment(self):
         for line in self.order_line:
